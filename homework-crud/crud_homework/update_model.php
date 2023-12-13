6fdabe975f7468fedf8b0672964376634d192d95
ThanhVan25 <van.tran25@student.passerellesnumeriques.org>
2023-12-13 13:24:56 +0700

    first commit

---
+++ homework-crud/crud_homework/update_model.php
@@ -0,0 +1,23 @@
+<?php
+include 'database/database.php';
+
+$name = $_POST['name'];
+$age = $_POST['age'];
+$email = $_POST['email'];
+$image_url = $_POST['image_url'];
+$id = $_POST['id'];
+
+$value = array(
+    'name' => $name,
+    'age' => $age,
+    'email' => $email,
+    'profile' => $image_url,
+    'id' => $id
+);
+
+updateStudent($value);
+header('Location: index.php');
+
+if (isset($_POST['submit'])){
+    exit();
+}
